bb053c5182504f108e5aeebecbc057b73baeb245
Ulric <ulricaird1@gmail.com>
2026-04-24 00:50:21 -0400

    changes

--- frontend/components/ui/carousel.tsx
+++ frontend/components/ui/carousel.tsx
@@ -95,11 +95,13 @@ function Carousel({
 
   React.useEffect(() => {
     if (!api) return
-    onSelect(api)
     api.on("reInit", onSelect)
     api.on("select", onSelect)
 
+    const timer = requestAnimationFrame(() => onSelect(api))
+
     return () => {
+      cancelAnimationFrame(timer)
       api?.off("select", onSelect)
     }
   }, [api, onSelect])
