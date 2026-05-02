9bab09e677d0d215aeae6b72df8485c4bd78e3e3
Ulric <ulricaird1@gmail.com>
2026-05-01 20:16:08 -0400

    feat(backend): Fixing Critical Issue

--- backend/src/auth/dto/refresh-token.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class RefreshTokenDto {
-  @ApiProperty({ example: 'v1.MDA4OWE3ZT...' })
-  @IsString()
-  @IsNotEmpty()
-  refresh_token!: string;
-}
