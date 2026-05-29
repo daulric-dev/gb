884efdcba117971895558f3b25c0b8f754e27aa6
Ulric <ulricaird1@gmail.com>
2026-05-28 20:08:25 -0400

    feat(backend): Diffrenciating Between Frontend and Backend (#123)
    * feat(backend): Diffrenciating Between Frontend and Backend
    
    * feat: Package Update
    
    * feat: Lock Update

--- backend/src/createApp.ts
+++ backend/src/createApp.ts
@@ -1,10 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module.js';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import multipart from '@fastify/multipart';
 import cookie from '@fastify/cookie';
@@ -21,18 +18,23 @@ export async function createApp(): Promise<NestFastifyApplication> {
     limits: { fileSize: 10 * 1024 * 1024 },
   });
 
-  const config = new DocumentBuilder()
-    .setTitle('gbv2 api')
-    .setDescription('api doc')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
+  
+  console.log(`Running in ${process.env.NODE_ENV} mode`);
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  if (process.env.NODE_ENV !== 'production') {
+      const config = new DocumentBuilder()
+        .setTitle('gb api')
+        .setDescription('api docs')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+  }
+
 
   app.setGlobalPrefix('api');
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -40,6 +42,16 @@ export async function createApp(): Promise<NestFastifyApplication> {
       transform: true,
     }),
   );
+
+  const raw_instance = app.getHttpAdapter().getInstance();
+
+  raw_instance.get("/", (req, res) => {
+    res.send("gb for life");
+  });
+
+  raw_instance.get("/health", (req, res) => {
+    res.status(200).send("ok");
+  });
 
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
@@ -49,6 +61,5 @@ export async function createApp(): Promise<NestFastifyApplication> {
   });
 
   await app.init();
-
   return app;
 }
